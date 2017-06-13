806effeeb3c8e25f84ce04d90ceb781fb6cabc72
Sebastian Bäcker <seb.b92@web.de>
2017-06-13 09:52:19 +0200

    Initial Commit

---
+++ src/main/java/de/thm/ews/JaxRSInit.java
@@ -0,0 +1,11 @@
+package de.thm.ews;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by sebb9 on 13.06.2017.
+ */
+@ApplicationPath("rs")
+public class JaxRSInit extends Application {
+}
---
+++ src/main/java/de/thm/ews/model/TestEntity.java
@@ -0,0 +1,7 @@
+package de.thm.ews.model;
+
+/**
+ * Created by sebb9 on 13.06.2017.
+ */
+public class TestEntity {
+}
