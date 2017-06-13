41e0e2bf06b6073b0746b6cf9bd627dfb2e04be8
Sebastian Bäcker <seb.b92@web.de>
2017-06-13 10:07:35 +0200

    TestService mit Echo-Ausgabe

--- src/main/java/de/thm/ews/service/TestService.java
+++ src/main/java/de/thm/ews/service/TestService.java
@@ -1,7 +1,18 @@
 package de.thm.ews.service;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
 /**
  * Created by sebb9 on 13.06.2017.
  */
+@Path("test")
 public class TestService {
+
+
+    @GET
+    public String echoTest() {
+        return "Test";
+    }
+
 }
